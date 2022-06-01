dc10b83333e3ecba985fda9b3856a4ce4957da34
Christopher Adamson <software.pro.08@gmail.com>
2022-06-01 14:47:37 -0700

    added config.yml for circle ci added output from run_docker added commands to upload_docker

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=eisel98/p4mlapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username eisel98
+docker tag p4mlapp $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
